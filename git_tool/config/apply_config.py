02d31f2bc08dace20ac2830d1a00fef25b500eab
Tabea Roethemeyer <tabea.roethemeyer@rub.de>
2024-07-31 11:01:04 +0200

    Remove config files
    The configuration process is not needed as previously expected. Whereever the code is executed, the most fitting git repo is found and one works form there

--- git_tool/config/apply_config.py
+++
@@ -1,4 +0,0 @@
-from git_tool.config.parse_and_validate import Config
-
-
-def apply_config_to_code(config: Config): ...
--- git_tool/config/parse_and_validate.py
+++
@@ -1,8 +0,0 @@
-Config = str  # TODO represent the schema of a configuration
-
-
-def parse_configuration_file(filename) -> Config: ...
-
-
-def validate_configuration(config: Config) -> bool:
-    return False
