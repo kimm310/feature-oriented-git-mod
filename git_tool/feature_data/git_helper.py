9040f4751d67895a4ec6cd4df68bbfaf98a2b437
Tabea Roethemeyer <tabea.roethemeyer@rub.de>
2024-08-14 07:58:13 +0200

    Fix wrong repo ref

--- git_tool/feature_data/git_helper.py
+++ git_tool/feature_data/git_helper.py
@@ -1,23 +1,27 @@
-from git import Repo
+from git_tool.feature_data.models_and_context.repo_context import repo_context
 
 
-def get_branches_for_commit(repo: Repo, commit_id: str) -> set[str]:
+def get_branches_for_commit(commit_id: str) -> set[str]:
     """
     Get all branches that contain the given commit.
     """
-    branches = repo.git.branch("--contains", commit_id).split("\n")
-    return {branch.strip() for branch in branches}
+
+    with repo_context() as repo:
+        branches = repo.git.branch("--contains", commit_id).split("\n")
+        return {branch.strip() for branch in branches}
 
 
-def get_author_for_commit(repo: Repo, commit_id: str) -> str:
+def get_author_for_commit(commit_id: str) -> str:
     """
     Get the author of the given commit.
     """
-    return repo.git.show("-s", "--format=%an", commit_id).strip()
+    with repo_context() as repo:
+        return repo.git.show("-s", "--format=%an", commit_id).strip()
 
 
-def get_files_for_commit(repo: Repo, commit_id: str) -> list[str]:
+def get_files_for_commit(commit_id: str) -> list[str]:
     """
     Get the list of files modified by the given commit.
     """
-    return repo.git.show("--name-only", "--pretty=", commit_id).splitlines()
+    with repo_context() as repo:
+        return repo.git.show("--name-only", "--pretty=", commit_id).splitlines()
